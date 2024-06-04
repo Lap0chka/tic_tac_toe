bbe67fab014f0fb8025244decdeb9c7a11746dfd
Daniil <danya.tkachenko.1997@gmail.com>
2024-06-04 22:18:01 +0200

    04.06.2024

--- tictac.py
+++ tictac.py
@@ -1,9 +1,11 @@
 class TicTacToe:
     """Game TicTacToe"""
+
     def __init__(self, board_size=3):
         self.board_size = board_size
         self.board = [[cell for cell in range(row, row + board_size)]
                       for row in range(1, board_size ** 2, board_size)]
+        self.step = self.board_size ** 2
         self.game()
 
     def draw_board(self):
@@ -17,7 +19,8 @@ class TicTacToe:
         print(('_' * self.board_size) * self.board_size, )
 
     def game_step(self, field, current_player):
-        """Player step"""
+        """Players step"""
+        self.step -= 1
         x_or_y = ['Y', 'X'][current_player]
         for row in range(self.board_size):
             for col in range(self.board_size):
@@ -55,8 +58,10 @@ class TicTacToe:
 
         if won:
             x_or_y = ['Y', 'X'][current_player % 2 == 0]
-            self.draw_board()
             print(f'My congratulations. {x_or_y} WON !!!')
+            return True
+        if self.step == 0:
+            print(f"It's draw!")
             return True
         return False
 
@@ -67,23 +72,42 @@ class TicTacToe:
         return won
 
     @staticmethod
-    def play_again():
+    def quit_the_game():
+        print('Bye Bye Bye\nSee you latter!!!')
+        quit()
+
+    def play_again(self):
         """Start game again or not"""
         play_again = input('Play again? (y-yes): ').lower()
         if play_again == 'y':
             return True
         else:
-            print('Bye Bye Bye. See you later!')
+            self.quit_the_game()
             return False
+
+    def move(self):
+        max_number = self.board_size ** 2
+        while True:
+            self.draw_board()
+            try:
+                field = input('Choose your field (q latter to quit): ')
+                if field.lower() == 'q':
+                    self.quit_the_game()
+                field = int(field)
+                if field > max_number or field < 1:
+                    print(f'You can choose only number between [1, {max_number}]\nTry again')
+                return field
+            except ValueError:
+                print(f'You can choose only number\nTry again')
 
     def game(self):
         """Start game"""
         current_player = True
         while True:
-            self.draw_board()
-            field = int(input('Choose your field (q latter to quit): '))
+            field = self.move()
             current_player = self.game_step(field, current_player)
             if self.check_win(current_player):
+                self.draw_board()
                 if self.play_again():
                     self.__init__()
                 else:
